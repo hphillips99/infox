39364a644031f5734897f0b632a645656495eb90
Harry Phillips <hphillips@knox.edu>
2022-05-23 13:57:52 -0500

    added instructions link

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="myBlue" sticky="top" expand="md">
-        <Navbar.Brand>
+        <Navbar.Brand href="/" class="linkText">
           <img src={logo} width="40px" height="40px" />
           InfoX
         </Navbar.Brand>
-
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav>
-            <Nav.Link href="Login">Login</Nav.Link>
-            <Nav.Link href="History">History</Nav.Link>
-            <NavDropdown title="Settings">
+            <Nav.Link href="login">Login</Nav.Link>
+            <Nav.Link href="history">History</Nav.Link>
+            <NavDropdown title="settings">
               <NavDropdown.Item href="Settings/Lmode">Light/Dark Mode</NavDropdown.Item>
             </NavDropdown>
           </Nav>
